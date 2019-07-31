fe5b847da48c757e6105b9ccc48e7b93b32c888e
Jan Varmuža <jan.varmuza@gmail.com>
2019-07-31 22:03:42 +0200

    Updates

---
+++ precache-manifest.568e6f4cbc58f81484ba4d0489562e5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7f71e486ad750dff78631bbde6754c",
+    "url": "/table-tennis-matches-2019/index.html"
+  },
+  {
+    "revision": "073f507ced201a5efd8f",
+    "url": "/table-tennis-matches-2019/static/css/main.00973089.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/table-tennis-matches-2019/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "073f507ced201a5efd8f",
+    "url": "/table-tennis-matches-2019/static/js/main.0ac60520.chunk.js"
+  },
+  {
+    "revision": "7292a981a192925c972c",
+    "url": "/table-tennis-matches-2019/static/js/runtime~main.29eaf3e6.js"
+  }
+]);
