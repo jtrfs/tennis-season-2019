d3383320dd59accb7e7a9a38f64645840772ca01
Jan Varmuža <jan.varmuza@gmail.com>
2019-08-02 14:35:41 +0200

    Updates

--- precache-manifest.cf5f037f00f035c0277ce08fef15baf9.js
+++ precache-manifest.cf5f037f00f035c0277ce08fef15baf9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bd111e21c3bdada46bf1d3eeb2a40292",
+    "revision": "78bf1948dca443d749312a3431e4f759",
     "url": "/tennis-season-2019/index.html"
   },
   {
-    "revision": "e92eae447b27fde1431d",
-    "url": "/tennis-season-2019/static/css/main.b5a28fcd.chunk.css"
+    "revision": "fa7091613fa4b4c757f2",
+    "url": "/tennis-season-2019/static/css/main.a13059a6.chunk.css"
   },
   {
     "revision": "681b51057d74cfa8975d",
     "url": "/tennis-season-2019/static/js/2.65aa1cca.chunk.js"
   },
   {
-    "revision": "e92eae447b27fde1431d",
-    "url": "/tennis-season-2019/static/js/main.49ff447c.chunk.js"
+    "revision": "fa7091613fa4b4c757f2",
+    "url": "/tennis-season-2019/static/js/main.227fe086.chunk.js"
   },
   {
     "revision": "4dce857ce916fc6b2e81",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tennis-season-2019/precache-manifest.8edfbcf8720180decf8513f339ee0d77.js"
+  "/tennis-season-2019/precache-manifest.cf5f037f00f035c0277ce08fef15baf9.js"
 );
 
 self.addEventListener('message', (event) => {
